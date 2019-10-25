82bb81ca53a60ceabf4896853248171a79f107e1
Jacob Lara <l1a9r9a7@gmail.com>
2019-10-24 21:14:06 -0700

    Changes to js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,15 @@
 
+var input = $(".text").text();
+var save = $(".save");
+
 var time = $("#date-time").text(moment().format("dddd, MMMM Do YYYY"));
+
+
+$("button").on("click", function(event) {
+    event.preventDefault();
+
+    localStorage.setItem("text", input);
+    
+})
+
+alert(localStorage.getItem("text"));
